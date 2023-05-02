fc9feeff3a0b3b964688894bb98a941f5872f02e
Mislav Kapetan <mkapetan20@student.foi.hr>
2023-05-02 20:55:15 +0200

    Rjesenje

--- Evaluation Manager (3)/Models/Person.cs
+++ Evaluation Manager (3)/Models/Person.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Evaluation_Manager.Models {
-    public class Person {
+namespace Evaluation_Manager.Models
+{
+    public class Person
+    {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return FirstName + " " + LastName;
         }
     }
--- Evaluation Manager (3)/Models/Teacher.cs
+++ Evaluation Manager (3)/Models/Teacher.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Evaluation_Manager.Models {
-    public class Teacher : Person {
+namespace Evaluation_Manager.Models
+{
+    public class Teacher : Person
+    {
         public string Username { get; set; }
         public string Password { get; set; }
 
